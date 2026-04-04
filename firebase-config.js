116c8d0c503ee59be1eacafdfbed3a911fc3888d
moviemate1 <yadavvenom9005@gmail.com>
2026-04-05 00:02:38 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBBmZHDicqT-7DbSF7ZGMdPJhCCP6fFlEY",
+  authDomain: "moviemate-57ddb.firebaseapp.com",
+  projectId: "moviemate-57ddb",
+  storageBucket: "moviemate-57ddb.firebasestorage.app",
+  messagingSenderId: "1098626760598",
+  appId: "1:1098626760598:web:a21b50f96b1e62323c27b7",
+  measurementId: "G-J0RLLD532E"
+};
