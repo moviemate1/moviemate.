43cd03ab612e434aa4cb50a35a8817540501a4e8
moviemate1 <yadavvenom9005@gmail.com>
2026-06-09 14:04:11 +0545

    Add files via upload

--- explore/app.js
+++ explore/app.js
@@ -2335,7 +2335,7 @@ function renderCuratedExploreRows(titles) {
     if (useMobileInterestLayout) {
       trendingHeading.innerHTML = `
         <span class="trend-heading-icon" aria-hidden="true">
-          <svg viewBox="0 0 24 24"><path d="M13.2 2.6c1.6 2.1 2.4 4 2.4 6 0 1.7-.6 3.2-1.8 4.4.3-1.2.1-2.4-.6-3.4-.6-.8-1.4-1.4-2-2.3-.6.8-1 1.8-1 3 0 2.4 1.8 4.4 4.1 4.8-3.6.6-6.9-1.7-7.6-5.3-.6-3.1.8-6.1 3.3-8 0 0 1.2 3.3 4 4.9-.1-1.6-.4-3.1-.8-4.1Z"></path></svg>
+          <svg viewBox="0 0 24 24"><path d="M12.8 2.8c1.7 2.1 2.6 4.2 2.6 6.5 0 1.9-.7 3.5-2 4.8.2-1.3 0-2.5-.7-3.6-.6-.9-1.5-1.6-2.1-2.5-.7.9-1.2 2-1.2 3.4 0 2.8 1.8 4.8 4.4 5.2-3.8.8-7.2-1.7-8-5.5-.6-3.2.8-6.4 3.5-8.4 0 0 1.3 3.5 4.1 5.1-.1-1.8-.4-3.5-.6-5Z"></path></svg>
         </span>
         <span class="trend-heading-text">Most Interested</span>
       `;
